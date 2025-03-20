1dcf515c9fcd48d2977699d4385463a26e058958
julius1183 <juliocesarcordero_1983@hotmail.com>
2025-03-19 23:35:23 -0500

    primer commit agrego el datoscompuestos.py

---
+++ datoscompuestos.py
@@ -0,0 +1,2 @@
+lista = ["Julio Cordero","Soy Julio",True,1.68]
+print(lista[0])
---
+++ variables.py
@@ -0,0 +1,19 @@
+#definiendo una variable con snake_case
+nombre_completo_de_tu_tio= "julio"
+#definiendo una variable
+nombre= "julio"
+
+#concatenar cin f-strings
+vienvenida= f"hola {nombre} como estas"
+
+
+#concatenar con +
+vienvenida = "hola" + "como estas"
+
+
+#operadores de pertenencia (in) (not in)
+print("juan"  in vienvenida) #true
+print("juan" not in vienvenida) #false
+
+
+
