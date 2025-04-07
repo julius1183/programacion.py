bd34201493c1b22c05577a9df032e5c1fc271ed4
julius1183 <juliocesarcordero_1983@hotmail.com>
2025-04-07 00:46:31 -0500

    operadores.py

---
+++ operadores.py
@@ -0,0 +1,22 @@
+#suma y resta (+ y -)
+suma = 12 + 5
+resta= 12 - 5
+
+#multi y division (* y /)
+multi = 12 *5
+division = 12 / 6 #devuelve un dato float (flotante)
+
+#potenciacion (exponente) (**)
+exponente = 12 **5
+
+#divicion baja (//) devuelve entero redondeando hacia abajo
+division_baja = 22//5
+
+#resto o modulo
+resto = 12 % 5
+
+print(division)
+
+tipo_de_dato = type("hola maestro") #type(dato) nos indica que tipo de dato es
+
+print(tipo_de_dato) 
